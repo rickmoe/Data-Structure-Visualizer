05ae14cdaecc7297f6e5d68c979816e4eb98f546
rickmoe <Cosmic.Star@comcast.net>
2023-07-10 23:59:25 -0500

    Refined design and styling

--- src/pages/array/Array.jsx
+++ src/pages/array/Array.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import "./Array.css";
 
 const Array = () => {
   return (
     <>
       <h1>Array</h1>
+      <p>
+        An array stores multiple values or other data structures (called{" "}
+        <em>elements</em>) next to each other. Each element of an array can be
+        accessed and changed independent of the other indices. Individual
+        elements can be selected by their <em>index</em> in the array, or their
+        position in the array if you were to count starting at 0.
+      </p>
     </>
   );
 };
